f752fe955816484afc517c394df6c644ac7f8391
Navid <navidsotoudehmehr@gmail.com>
2022-04-11 23:12:53 +0430

    Delete and Update and Create for snippets is created and auoth is coming soon

---
+++ README.md
@@ -0,0 +1,21 @@
+Source code for the [Next.js](https://nextjs.org/) crash course project that uses Tailwind CSS, SWR, React Hook Form, and FaunaDB!
+
+![Working Demo](./images/cover.jpg)
+
+## Getting Started
+
+1. [Sign up for a free account at FaunaDB](http://bit.ly/jqqfauna).
+
+2. Copy the `.env.local.example` file to `.env.local` and fill in your `FAUNA_SECRET`.
+
+3. Create a collection in Fauna called `snippets`.
+
+4. Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import Snippet from '../components/Snippet';
+import useSWR from 'swr';
+import Link from 'next/link';
+export default function Home() {
+    const { data: snippets, mutate } = useSWR('/api/snippets');
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="">
+                <div className="my-12">
+                    <h1 className="text-red-100 text-2xl">
+                        Errday Code Snippets
+                    </h1>
+                    <p className="text-red-200">
+                        Create and browse snippets you use every day in Web
+                        Development!
+                    </p>
+                    <Link href="/new">
+                        <a className="mt-3 inline-block bg-red-800 hover:bg-red-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Create a Snippet!
+                        </a>
+                    </Link>
+                </div>
+                {snippets &&
+                    snippets.map((snippet) => (
+                        <Snippet
+                            key={snippet.id}
+                            snippet={snippet}
+                            snippetDeleted={mutate}
+                        />
+                    ))}
+            </main>
+        </div>
+    );
+}
